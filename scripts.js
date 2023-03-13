ea816f06ba9e389b63299fe54933a47747cd0d2e
Bruggoli <99993823+Bruggoli@users.noreply.github.com>
2023-03-13 14:54:09 +0100

    Changed folder structure, added json functionality

--- scripts.js
+++ scripts.js
@@ -27,26 +27,65 @@ function storeCache(info) {
         console.log("Cache created");
     }
 
-    splitUsers();
+    //splitUsers();
 }
-
-function splitUsers() {
-    let csv = localStorage.getItem("loddtrekning-data-cache");
-    if (csv) {
-        let splitData = csv.split(" ");
-        let tabell = [];
-        for (i in splitData) {
-            console.log(splitData[i]);
-            let persInfo = splitData[i].split(";");
-            console.log(persInfo[0], persInfo[1], persInfo[2]);
-        };
-    };
-};
 
 function copyClipboard() {
     // Copy the text inside localstorage
-    navigator.clipboard.writeText(localStorage.getItem("loddtrekning-data-cache"));
+    navigator.clipboard.writeText(localStorage.getItem("Navneliste"));
 
     // Alert the copied text
-    alert("Copied the text: " + localStorage.getItem("loddtrekning-data-cache"));
+    alert("Copied the text: " + localStorage.getItem("Navneliste"));
+}
+
+function jsonStorage() {
+    var navn = [{
+        "name": ["Markus Moen Magnussen",
+            "Kaisa Lien",
+            "Adrian Indergård Ro",
+            "Marius Jølsen Lindberg"]
+    },
+    {
+        "lodd": [1, 1, 1, 1]
+    },
+    {
+        "vunnet": [0, 0, 0, 0]
+    }];
+
+    // console.log("typeof testObject: " + typeof navn);
+    // console.log("testObject properties: ");
+
+
+    localStorage.setItem("Navneliste", JSON.stringify(navn));
+
+    var retrievedObject = JSON.parse(localStorage.getItem("Navneliste"));
+
+    // console.log('typeof retrievedObject: ' + typeof retrievedObject);
+    // console.log('Value of retrievedObject: ' + retrievedObject);
+}
+
+function fillTable() {
+    var json = JSON.parse(localStorage.getItem("Navneliste"));
+    const size = json[0].length;
+    console.log("filltable test 1: " + json[0].name[0]);
+    var table = document.getElementById("loddtable");
+    
+    for (var i = 0; i < json[0].name.length; i++) {
+        var newRow = document.createElement("tr");
+        console.log(json[0].name[i]);
+        var nameCell = document.createElement("td");
+        nameCell.textContent = json[0].name[i];
+        var initCell = document.createElement("td");
+        let nameArr = json[0].name[i].split(" ");
+        var initials = "";
+        for (n in nameArr) {
+            initials += nameArr[n].slice(0, 1);
+        }
+        initCell.textContent = initials;
+        console.log("init " + initials);
+
+        newRow.appendChild(initCell);
+        newRow.appendChild(nameCell);
+        table.getElementsByTagName("tbody")[0].appendChild(newRow);
+    }
 }
