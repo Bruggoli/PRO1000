209a5a6109e8cac4371c436bcdf67ac67907138a
Bruggoli <99993823+Bruggoli@users.noreply.github.com>
2023-04-23 14:12:27 +0200

    Main

--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
+const { Session } = require("express-session");
+
 function loadFile() {
     document.getElementById('file').onchange = () => {
         const file = document.getElementById('file').files[0];
@@ -17,14 +19,14 @@ function loadFile() {
 }
 
 function storeCache(info) {
-    let oldData = localStorage.getItem("loddtrekning-data-cache");
+    let oldData = localStorage.getItem("Navneliste");
     if (oldData) {
         // todo if exists
-        localStorage.setItem("loddtrekning-data-cache", info);
-        console.log("Cache updated");
+        localStorage.setItem("Navneliste", info);
+        console.log("LocalStorage Updated");
     } else {
-        localStorage.setItem("loddtrekning-data-cache", info);
-        console.log("Cache created");
+        localStorage.setItem("Navneliste", info);
+        console.log("LocalStorage Created");
     }
 
     //splitUsers();
@@ -39,64 +41,233 @@ function copyClipboard() {
 }
 
 function jsonStorage() {
+    // change to get names from json file
     var navn = [{
-        "name": ["Markus Moen Magnussen",
-            "Kaisa Lien",
-            "Adrian Indergård Ro",
-            "Marius Jølsen Lindberg"]
-    },
-    {
-        "lodd": [1, 1, 1, 1]
-    },
-    {
-        "vunnet": [0, 0, 0, 0]
+        "name": [   "Anne Gry Sturød",    
+                    "Amalie H. Røimål",    
+                    "Anette Myhre Momrak",    
+                    "Asbjørn Nygård",    
+                    "Anh Nguyen Duc",    
+                    "Aud Stokland",    
+                    "Bjørn Kristoffersen",    
+                    "Birgit Leick",    
+                    "Christian Persson",    
+                    "Dieu Tien Bui",    
+                    "Erik Aarnes",    
+                    "Endre Før Gjermundsen",    
+                    "Elisabeth Fleseland",    
+                    "Einar Mo",    
+                    "Gjest 1",    
+                    "Gjest 2",    
+                    "Gudrun Helgadottir",    
+                    "Gunn Kristin Leikvoll",    
+                    "Helge Gjermund Kaasin",    
+                    "Håkon Svendsen",    
+                    "Ingeborg Nordbø",    
+                    "Ingrid Sundbø",    
+                    "Jon Kvisli",    
+                    "Karl Johan Gloppen",    
+                    "Lars Halvor O. Johnsen",    
+                    "Lars Sanden",    
+                    "Martin Falk",    
+                    "Mesay Moges Menebo",    
+                    "Nuno Marques",    
+                    "Niklas Valter Kreander",    
+                    "Per Christian Hagen",    
+                    "Per Strømberg",    
+                    "Runar Gundersen",    
+                    "Roy Martin Istad",    
+                    "Sigbjørn Hjelmbrekke",    
+                    "Safiqul Islam",    
+                    "Simone",    
+                    "Terje Andersen",    
+                    "Tor Lønnestad",    
+                    "Øystein A. Wendelborg",    
+                    "Hege Skogli Riege"]
     }];
-
-    // console.log("typeof testObject: " + typeof navn);
-    // console.log("testObject properties: ");
 
 
     localStorage.setItem("Navneliste", JSON.stringify(navn));
-
-    var retrievedObject = JSON.parse(localStorage.getItem("Navneliste"));
-
-    // console.log('typeof retrievedObject: ' + typeof retrievedObject);
-    // console.log('Value of retrievedObject: ' + retrievedObject);
 }
 
-function fillTable() {
-    var json = JSON.parse(localStorage.getItem("Navneliste"));
-    const size = json[0].length;
+function fillTable(tableIn, jsonIn) {
+    var json = JSON.parse(localStorage.getItem(jsonIn));
     console.log("filltable test 1: " + json[0].name[0]);
-    var table = document.getElementById("loddtable");
+    var table = document.getElementsByClassName(tableIn);
     
     for (var i = 0; i < json[0].name.length; i++) {
         var newRow = document.createElement("tr");
+        newRow.setAttribute("id", "row" + i);
         console.log(json[0].name[i]);
         var nameCell = document.createElement("td");
+        nameCell.setAttribute("id", "navn" + i);
         nameCell.textContent = json[0].name[i];
-        var initCell = document.createElement("td");
+        
         let nameArr = json[0].name[i].split(" ");
+        
+        var initCell = document.createElement("td");
         var initials = "";
         for (n in nameArr) {
             initials += nameArr[n].slice(0, 1);
         }
+        initCell.setAttribute("id", "init" + i);
         initCell.textContent = initials;
         console.log("init " + initials);
-
+        var loddCell = document.createElement("input");
+        
+        loddCell.setAttribute("type", "number");
+        loddCell.setAttribute("id", "inputRow"+ i);
+        loddCell.setAttribute("class", "inputLodd");
+        
         newRow.appendChild(initCell);
         newRow.appendChild(nameCell);
-        table.getElementsByTagName("tbody")[0].appendChild(newRow);
+        newRow.appendChild(loddCell);
+        document.getElementById("tableBody").appendChild(newRow);
     }
 }
+/*
+function giLodd() {
+    var navneliste = document.getElementById("tableBody");
+    var deltakere = document.getElementById("tableBodyLeft");
+    var rad = 0;
+    var lodd = 0;
+
+    // this is the loop that goes through the table
+    // and checks if the input is 0 or not
+    // if it is 0, it skips the row
+    // if it is not 0, it adds the name and lodd to the tableBodyLeft
+    for (var i = 0; i < navneliste.rows.length; i++) {
+        if (document.getElementById("inputRow" + i).value <= 0) {
+            // console.log("ingen lodd");
+        } else {
+            for (var i = 0; i < document.getElementById("inputRow" + i).value; i++) {
+                var navn = document.getElementById("navn" + i).textContent;
+                var lodd = document.getElementById("inputRow" + i).value;
+
+                // overfør lodd og navn til tableBodyLeft
+                var newRow = document.createElement("tr");
+                newRow.setAttribute("id", "spillerRow" + rad);
+
+                var loddCell = document.createElement("td");
+                loddCell.setAttribute("id", "spillerLodd" + rad);
+                loddCell.textContent = lodd++;
+                // console.log("giLodd test 2: " + lodd);
+
+                var initCell = document.createElement("td");
+                initCell.textContent = document.getElementById("init" + i).innerHTML;
+
+                var nameCell = document.createElement("td");
+                nameCell.setAttribute("id", "spillerNavn" + rad);
+                nameCell.textContent = navn;
+                //console.log("giLodd test 1: " + navn);
+                
+                newRow.appendChild(nameCell);
+                newRow.appendChild(initCell);
+                newRow.appendChild(loddCell);
+                deltakere.appendChild(newRow);
+                rad++;
+            }
+        }
+    }
+
+    console.log("lodd tildelt");
+}
+*/
 
 function giLodd() {
+    clearTableBodyLeft();
+    var navneliste = document.getElementById("tableBody");
+    var deltakertabell = document.getElementById("tableBodyLeft");
+    var deltakere = [];
+    var lodd = 0;
+    var rad = 0;
+    
+    if (sessionStorage.getItem("loddnr") != null) {
+        var loddnr = 0;
+    } else {
+        var loddnr = sessionStorage.getItem("loddnr");
+    }
 
+    for (var i = 0; i < navneliste.rows.length; i++) {
+        var navnRow = document.getElementById("navn" + i);
+        var inputRow = document.getElementById("inputRow" + i)
+        if (document.getElementById("inputRow" + i).value > 0) {
+            console.log("giLodd test 2: " + navnRow.textContent);
+            console.log("giLodd test 1: " + inputRow.value);
+            var deltaker = {
+                'navn': navnRow.textContent,
+                'lodd': inputRow.value
+            };
+            deltakere.push(deltaker);
+            lodd += parseInt(inputRow.value);
+            console.log("lodd tildelt: " + lodd);
+            sessionStorage.setItem("deltakere", JSON.stringify(deltakere));
+        }
+    }
+
+    for (var i = 0; i < deltakere.length; i++) {
+        for (var j = 1; j <= deltakere[i].lodd; j++) {
+            console.log("deltaker", i, "navn: " + deltakere[i].navn + ", lodd: " + loddnr++);
+            var newRow = document.createElement("tr");
+            newRow.setAttribute("id", "deltakerRow" + rad);
+    
+            var loddCell = document.createElement("td");
+            loddCell.setAttribute("id", "deltakerLodd" + rad);
+            loddCell.textContent = loddnr; // Set loddCell textContent to the current ticket number
+    
+            var nameCell = document.createElement("td");
+            nameCell.setAttribute("id", "deltakerNavn" + rad);
+            nameCell.textContent = deltakere[i].navn;
+        
+            newRow.appendChild(nameCell);
+            newRow.appendChild(loddCell);
+            deltakertabell.appendChild(newRow);
+            rad++;
+        }
+    }
+    sessionStorage.setItem("loddnr", loddnr);
 }
 
-function trekning() {
-    var navn = JSON.parse(localStorage.getItem("Navneliste"));
-    var vinner = document.getElementById("vinner");
-    let deVant = navn[0].name[0];
-    vinner.innerText = deVant;
-}
+
+
+function velgVinner() {
+    var deltakertabell = document.getElementById("tableBodyLeft");
+    var numDeltakere = deltakertabell.rows.length;
+    
+    if (numDeltakere == 0) {
+      alert("Ingen deltakere igjen i raffle!");
+      return;
+    }
+    
+    // Choose a random winner index from deltakertabell
+    var winnerIndex = Math.floor(Math.random() * numDeltakere);
+    
+    // Get the winning row from deltakertabell
+    var winningRow = deltakertabell.rows[winnerIndex];
+    
+    // Get the winning ticket number from loddCell in the winning row
+    var loddCell = winningRow.querySelector("td:nth-child(2)"); // Assuming loddCell is the second <td> in each row
+    var winningTicket = loddCell.textContent;   
+    
+    // Grey out the winning ticket number
+    loddCell.style.backgroundColor = "#ddd";
+    loddCell.style.color = "#888";
+    
+    // Remove the winning row from deltakertabell
+    deltakertabell.deleteRow(winnerIndex);
+    
+    // Return the name of the winner and their winning ticket number
+    var nameCell = winningRow.querySelector("td:nth-child(1)"); // Assuming nameCell is the first <td> in each row
+    var winnerName = nameCell.textContent;
+
+    alert(winnerName + " vant med lodd#" + winningTicket + "!");
+    return winnerName + ", " + winningTicket;
+  }
+  
+
+  function clearTableBodyLeft() {
+    var deltakertabell = document.getElementById("tableBodyLeft");
+    while (deltakertabell.firstChild) {
+      deltakertabell.removeChild(deltakertabell.firstChild);
+    }
+  }
